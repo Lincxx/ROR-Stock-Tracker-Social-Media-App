498713a27c6c18e38f45147fc448502f81ecd118
Lincxx <bossco28@gmail.com>
2022-01-29 11:14:42 -0500

    Add view peofile, list friend tracked stock,  friends result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     # current_user come from the devise gem
     @tracked_stocks = current_user.stocks
   end
@@ -7,6 +8,11 @@ class UsersController < ApplicationController
 
   def my_friends
     @friends = current_user.friends
+  end
+
+  def show 
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
   def search
